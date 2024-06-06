916cf19d52370a8c430531676699e1fa9ca599ff
20232543 <shdh004@ess.ais.ac.nz>
2024-06-06 12:10:50 +1200

    Installation Done

---
+++ chat/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Chatroom(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    text = models.TextField()
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text + "-" + self.user.username
