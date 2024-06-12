36887a326d0d375531a519288293b944636f9e27
20232543 <shdh004@ess.ais.ac.nz>
2024-06-13 10:33:09 +1200

    get ready for Vercel, also I did my homework

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,3 +5,9 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chatroom
         fields = ['id', 'name']
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id','name']
+        
